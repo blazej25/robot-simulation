d67d32f999d5f0ab5620dd3d05e94f42b73a6428
bkubica25 <bkubica25@gmail.com>
2023-02-20 21:29:38 +0100

    Palindromes

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print("Hello world")
+def palindrome(num: str):
+    revnum = num[::-1]
+
+    if not revnum == num:
+        num = int(revnum) + int(num)
+        palindrome(str(num))
+    else:
+        print(num) 
+
+if __name__ == '__main__':
+    num = input('Please input your number.\n')
+    palindrome(num)
